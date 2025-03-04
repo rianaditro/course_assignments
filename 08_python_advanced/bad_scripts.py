e2e5e677cc72cf8ee94e1f990e3908f1566afe3b
khilmi-aminudin <khilmiaminudin715@gmail.com>
2025-03-04 17:08:05 +0700

    fix: fix bad_script.py

--- 08_python_advanced/bad_scripts.py
+++ 08_python_advanced/bad_scripts.py
@@ -2,23 +2,24 @@
 
 import os,sys  # Multiple imports on one line (violates PEP 8)
 
-def badFunction  ( name ,age ): # Incorrect spacing, bad function name casing
-    if age>18:print("Welcome " +name) # No indentation, missing spaces, bad string concatenation
+def good_function(name, age): # Incorrect spacing, bad function name casing
+    if age>18:
+      print("Welcome " + name) # No indentation, missing spaces, bad string concatenation
     else:
-     print("Sorry, " + name + " you are not allowed.") # Inconsistent indentation
+      print("Sorry, " + name + " you are not allowed.") # Inconsistent indentation
     
-def    another_function():
-  print("This function has inconsistent indentation.") # Bad indentation
+def another_good_function():
+    print("This function has inconsistent indentation.") # Bad indentation
 
-class   Myclass: # Class name should be MyClass (PascalCase)
+class MyClass: # Class name should be MyClass (PascalCase)
   def __init__(self,x,y):
-    self.x=x # No space around operator
-    self.y= y  # Inconsistent spacing
+    self.x = x # No space around operator
+    self.y = y  # Inconsistent spacing
 
   def show_values( self ): # Extra spaces in parameter list
         print( "X:",self.x,"Y:", self.y ) # Inconsistent spacing
 
-myobj=Myclass(10,20) # Poor variable naming
-myobj.show_values()
+entry_level = MyClass(10,20) # Poor variable naming
+entry_level.show_values()
 
 badFunction("Alice", 17) # Function name should be snake_case
