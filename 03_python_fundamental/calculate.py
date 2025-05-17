2ee1947330d1616fb34525a4b9f533df726113fd
Denni Septiyaji <denni.septiyaji@gmail.com>
2025-05-17 17:36:21 +0700

    sdenni python fundamental

---
+++ 03_python_fundamental/calculate.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    """
+    Hitung luas persegi panjang.
+
+    :param length: Panjang persegi panjang
+    :param width: Lebar persegi panjang
+    :return: Luas persegi panjang
+    """
+    return length * width
---
+++ 03_python_fundamental/mymodule.py
@@ -0,0 +1,2 @@
+def greet_user(name):
+    return f"Hello, {name}! Welcome to my custom module."
